87714f86f1d20d70a879ec1da5d4674d052c89c8
dbr <dbr.onix@gmail.com>
2013-07-03 22:26:45 +0930

    stitle was deprecated in favour of title and --restrict-filenames

--- ytdl/tasks.py
+++ ytdl/tasks.py
@@ -39,9 +39,9 @@ def grab_video(videoid, force=False):
             raise
 
     # Get output filename
-    output_format = "%(upload_date)s_%(stitle)s%(ext)s_%(id)s"
+    output_format = "%(upload_date)s_%(title)s%(ext)s_%(id)s"
     p = subprocess.Popen(
-        ["youtube-dl", "--output", output_format, video.url, "--get-filename"],
+        ["youtube-dl", "--restrict-filenames", "--output", output_format, video.url, "--get-filename"],
         cwd = cwd,
         stdout = subprocess.PIPE)
     so, _se = p.communicate()
@@ -56,7 +56,7 @@ def grab_video(videoid, force=False):
     filename = os.path.join(cwd, so.strip())
 
     p = subprocess.Popen(
-        ["youtube-dl", "--output", "%(upload_date)s_%(stitle)s_%(id)s.%(ext)s", video.url],
+        ["youtube-dl", "--restrict-filenames", "--output", "%(upload_date)s_%(title)s_%(id)s.%(ext)s", video.url],
         cwd = cwd)
 
     p.communicate()
