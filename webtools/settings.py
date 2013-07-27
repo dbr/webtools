476ea8b7ee145f9c8fdacc37225267bfa4b676c1
dbr <dbr.onix@gmail.com>
2013-07-27 21:33:42 +0930

    Add template-timing DDT pane

--- webtools/settings.py
+++ webtools/settings.py
@@ -130,6 +130,7 @@ INSTALLED_APPS = (
     'ytdl',
 
     'debug_toolbar',
+    'template_timings_panel',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -172,4 +173,24 @@ else:
     BROKER_URL = 'redis://localhost:6379/4'
     CELERY_RESULT_BACKEND = 'redis://localhost:6379/4'
 
+
+# Debug toolbar
 INTERNAL_IPS = ('127.0.0.1',)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'HIDE_DJANGO_SQL': True,
+    'TAG': 'div',
+    'ENABLE_STACKTRACES' : True,
+}
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'template_timings_panel.panels.TemplateTimings.TemplateTimings',
+)
