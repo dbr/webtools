1d96a629da3f057006c7817ae8c757a5791be5f8
dbr <dbr.onix@gmail.com>
2013-07-27 20:34:08 +0930

    Simple script to remove duplicated videos

---
+++ ytdl/management/commands/dedupe.py
@@ -0,0 +1,24 @@
+import ytdl.models
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Write out channel list"
+
+    def handle(self, *args, **kwargs):
+        kill = '--kill' in args
+
+        if not kill:
+            print "Dry-run, specify --kill to delete dupes"
+
+        seen = set()
+        for video in ytdl.models.Video.objects.all():
+            # TODO: maybe only check for dupes in channel? Utterly unlikely
+            url = video.url
+            if url in seen:
+                print "Dupe %s (%s)" % (url, video.status)
+                if kill:
+                    video.delete()
+            else:
+                seen.add(url)
