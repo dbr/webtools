2a62b67ce6a88df2657ca843ca662cc4f3091bcc
dbr <dbr.onix@gmail.com>
2013-07-31 14:09:09 +0930

    Make settings overridable from settings.py

--- ytdl/ytdl_settings.py
+++ ytdl/ytdl_settings.py
@@ -2,5 +2,9 @@ import os
 from django.conf import settings
 
 
-OUTPUT_DIR = os.path.expanduser("~/Movies/ytdl")
-OUTPUT_FORMAT = "%(upload_date)s_%(title)s_%(id)s.%(ext)s"
+OUTPUT_DIR = getattr(
+    settings,
+    "YTDL_OUTPUT_DIR", os.path.expanduser("~/Movies/ytdl"))
+OUTPUT_FORMAT = getattr(
+    settings,
+    "OUTPUT_FORMAT", "%(upload_date)s_%(title)s_%(id)s.%(ext)s")
