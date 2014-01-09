eaad8c69a2601c90c8748bab8c2e00cfbf5e2022
dbr <dbr.onix@gmail.com>
2014-01-09 20:14:09 +1030

    Test-cases for VimeoApi also

--- ytdl/tests.py
+++ ytdl/tests.py
@@ -18,7 +18,7 @@ class YoutubeTest(TestCase):
 
     def test_icon(self):
         url = self.api.icon()
-        assert url.startswith("https://")
+        assert url.startswith("http://") or url.startswith("https://")
 
         # .jpg seems only option, but might as well randomly guess at other options
         assert url.endswith(".jpg") or url.endswith(".png") or url.endswith(".gif")
@@ -26,3 +26,30 @@ class YoutubeTest(TestCase):
     def test_title(self):
         title = self.api.title()
         assert title == "Rooster Teeth"
+
+
+class VimeoTest(TestCase):
+    def setUp(self):
+        from ytdl.vimeo_api import VimeoApi
+        self.api = VimeoApi("dbr")
+
+    def test_list_videos(self):
+        videos = list(self.api.videos_for_user(limit=1))
+
+        assert len(videos) > 0
+        for v in videos:
+            assert 'url' in v
+            assert 'title' in v
+            assert 'id' in v
+            assert 'vimeo.com' in v['url'], v['url']
+
+    def test_icon(self):
+        url = self.api.icon()
+        assert url.startswith("http://") or url.startswith("https://")
+
+        # .jpg seems only option, but might as well randomly guess at other options
+        assert url.endswith(".jpg") or url.endswith(".png") or url.endswith(".gif")
+
+    def test_title(self):
+        title = self.api.title()
+        assert title == "dbr", title
