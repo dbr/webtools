6e466c58f67cdf7eb82d14695746749c4d041ca6
dbr <dbr.onix@gmail.com>
2017-12-04 22:18:28 +1030

    Try as both username and channel ID
    Inefficient, should shuffle stuff around in DB

--- ytdl/youtube_api.py
+++ ytdl/youtube_api.py
@@ -17,8 +17,14 @@ class YoutubeApi(object):
             chanid = self.chanid)
         resp = requests.get(url)
         if len(resp.json()['items']) == 0:
-            log.warning("No items found at %s" % url)
-            return
+            log.warning("No items found at %s - trying as ID" % url)
+
+            url = "https://www.googleapis.com/youtube/v3/channels?key={apikey}&id={chanid}&part=contentDetails".format(
+                apikey = self.API_KEY,
+                chanid = self.chanid)
+            resp = requests.get(url)
+            if len(resp.json()['items']) == 0:
+                log.warning("No items found at %s either" % url)
 
         upload_playlist = resp.json()['items'][0]['contentDetails']['relatedPlaylists']['uploads']
 
@@ -78,8 +84,18 @@ class YoutubeApi(object):
         if len(items) > 0:
             return items[0]['snippet']
         else:
-            log.warning("No items found at %s" % (url))
-            return None
+            log.warning("No items found at %s - trying as ID" % (url))
+            url = "https://www.googleapis.com/youtube/v3/channels?key={apikey}&id={chanid}&part=snippet".format(
+                apikey = self.API_KEY,
+                chanid = self.chanid,
+                )
+            items = requests.get(url).json()['items']
+            if len(items) > 0:
+                return items[0]['snippet']
+            else:
+                log.warning("No items found at %s either" % (url))
+                return None
+
 
     def icon(self):
         snippet = self._chan_snippet()
